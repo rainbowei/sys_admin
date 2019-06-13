b9821c2ef22db71ee4e544ae7f4d0374d6622fef
System Administrator <root@haodoudeMacBook-Pro.local>
2019-06-13 17:12:48 +0800

    修改首页百分比返回总是0

--- asset/views.py
+++ asset/views.py
@@ -22,20 +22,22 @@ def host_add(requests):
 
 
 def index(requests):
+    total=float(Host.objects.count())
     upline=Host.objects.filter(status=0).count()
     dowline=Host.objects.filter(status=1).count()
     unknow=Host.objects.filter(status=2).count()
     faild=Host.objects.filter(status=3).count()
     backup=Host.objects.filter(status=4).count()
+    per_upline=round(upline/total*100,1)
+    per_downline=round(dowline/total*100,1)
+    per_unknow=round(unknow/total*100,1)
+    per_faild=round(faild/total*100,1)
+    per_backup=round(backup/total*100,1)
     server_num=Host.objects.filter(type_choice='server').count()
     network_num=Host.objects.filter(type_choice='networkdevice').count()
     store_num=Host.objects.filter(type_choice='storagedevice').count()
     sec_num=Host.objects.filter(type_choice='securitydevice').count()
     soft_num=Host.objects.filter(type_choice='software').count()
-
-
-
-
 
     return  render(requests,'index.html',locals())
 
